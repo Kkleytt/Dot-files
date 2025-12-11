870125643cfef356d85ab576f6f591789c4823cb
Kkleytt <kkleytt@gmail.com>
2025-12-11 21:46:28 +0300

    Organize file sctructure

--- private_dot_config/caelestia/cli/scripts/hyprland/keyhint/executable_script.sh
+++ private_dot_config/caelestia/cli/scripts/hyprland/keyhint/executable_script.sh
@@ -4,4 +4,4 @@ PATH_IMAGES="$HOME/.config/caelestia/store/keybinds"
 service="${1:-hyprland}"
 level="${2:-base}"
 
-kitty -e swayimg -f -s 2.2 "$PATH_IMAGES/$service/$level.png"
+kitty -e swayimg -f -s fit "$PATH_IMAGES/$service/$level.png"
